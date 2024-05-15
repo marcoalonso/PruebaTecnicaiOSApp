2ea4baf4a57babe304e8ebc4a90c43ded2937de8
Marco Alonso Rodriguez <mrodriguez@epica.solutions>
2024-05-14 20:50:36 -0600

    Initial Commit

---
+++ PruebaTecnicaiOS/PruebaTecnicaiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTecnicaiOSApp.swift
+//  PruebaTecnicaiOS
+//
+//  Created by Marco Alonso Rodriguez on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTecnicaiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
